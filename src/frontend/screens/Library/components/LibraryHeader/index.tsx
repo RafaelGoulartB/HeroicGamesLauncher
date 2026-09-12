1258719f1a9ae8b83c594c3f5043fba2792a1a6f
RafaGoulartB <rafagoulartb@gmail.com>
2026-09-12 01:48:51 -0300

    feat: add Playnite import interface

--- src/frontend/screens/Library/components/LibraryHeader/index.tsx
+++ src/frontend/screens/Library/components/LibraryHeader/index.tsx
@@ -5,6 +5,7 @@ import { GameInfo } from 'common/types'
 import LibraryContext from '../../LibraryContext'
 import './index.css'
 import AddGameButton from '../AddGameButton'
+import ImportPlayniteButton from '../ImportPlayniteButton'
 
 type Props = {
   list: GameInfo[]
@@ -36,6 +37,7 @@ export default React.memo(function LibraryHeader({ list }: Props) {
             : t('title.allGames', 'All Games')}
           <span className="numberOfgames">{numberOfGames}</span>
           <AddGameButton data-tour="library-add-game" />
+          <ImportPlayniteButton />
         </span>
         <ActionIcons />
       </div>
